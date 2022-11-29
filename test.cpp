4dfc17248f6e25a9a3942100638a34df79f70881
Bhanu <erpratap10@gmail.com>
2022-11-29 19:36:48 +0530

    first commit

---
+++ test.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int, int> pi;
+typedef unordered_map<int,int> mp;
+typedef map<int,int> m;
+#define mod 1e9+7
+#define pb push_back
+#define fs first
+#define s second
+#define f(i,s,e) for(int (i)=s ; (i) < e ; (i)++)
+#define cf(i,s,e) for(int (i)=s ; (i)<=e ; (i)++)
+#define rf(i,s,e) for(int (i)=s ; (i)>=e ; (i)--)
+
+
+void f1(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+}
+
+int main(){
+    f1();
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cout << "github push";
+   
+
+    return 0;
+}
