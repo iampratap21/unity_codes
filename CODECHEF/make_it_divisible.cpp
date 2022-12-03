fd18e2a39d7b81b9c0eeee7ab37d1eb8901def98
Bhanu <erpratap10@gmail.com>
2022-12-03 22:23:04 +0530

    first commit

---
+++ CODECHEF/make_it_divisible.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int, int> pi;
+typedef unordered_map<int,int> mp;
+typedef map<int,int> m;
+#define mod 1e9+7
+#define pb push_back
+#define fs first
+#define s second
+#define f(i,s,e) for(int (i)=s ; (i) < e ; (i)++)
+#define cf(i,s,e) for(int (i)=s ; (i)<=e ; (i)++)
+#define rf(i,s,e) for(int (i)=s ; (i)>=e ; (i)--)
+
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+        if(n==1) cout << 3 << endl;
+        else {
+            cout << 1;
+            for(int i=1 ; i<n-1 ; i++) cout << 0;
+            cout << 5 << endl;
+        }
+    }
+    
+    return 0;
+}
